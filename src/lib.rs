d4e7dd8c0fbf1b8551016fbb54f0a1529a9a7c68
Yoshida Shin <wbcchsyn@gmail.com>
2021-03-02 16:32:54 +0900

    First samples

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,97 @@
-/// # Examples
-///
-/// ```
-/// # #[macro_use] extern crate rust_macro_examples;
-///
-/// assert_eq!(0, count!());
-/// assert_eq!(1, count!(3));
-/// assert_eq!(2, count!(3, 5));
-/// ```
+#![feature(log_syntax)]
+
 #[macro_export]
-macro_rules! count {
-    () => {
-        0usize
+macro_rules! logging{
+    (@eval) => {};
+    (@eval $head:tt $($tail:tt)*) => {
+        log_syntax!($head);
+        logging!(@eval $($tail)*);
     };
-    ($($vals:stmt),*) => {count!($($vals)* ,)};
-    ($_head:stmt, $($tail:stmt),*) => {
-        1usize + count!($($tail)*)
+
+    ($($body:tt),*) => {
+        logging!(@eval $($body)*);
     };
 }
 
-// /// # Examples
-// ///
-// /// ```
-// /// # #[macro_use] extern crate rust_macro_examples;
-// ///
-// /// let my_vec: Vec<i32> = my_vec![1,2,3];
-// /// let vec: Vec<i32> = vec![1,2,3];
-// ///
-// /// assert_eq!(vec, my_vec);
-// /// ```
-// #[macro_export]
-// macro_rules! my_vec {
-//     (@count, $n:tt,) => { $n };
-//     (@count, $n:tt, $_head:tt, $($tail:tt),*) => {
-//         let n = $n;
-//         my_vec!(@count, n + 1, $($tail),*)
-//     };
-//     ($($vals:tt),*) => {
-//         let count = my_vec!(@count, 0, $($vals),*);
-//         let mut ret = Vec::with_capacity(count);
-//         {
-//             let v = $vals;
-//             ret.push(v);
-//         }*;
-//         ret
-//     };
-// }
+#[macro_export]
+macro_rules! count0 {
+    (@convert $_:tt) => { () };
+    ($($body:tt),*) => {
+        <[()]>::len(&[$(count0!(@convert $body)),*])
+    };
+}
+
+#[macro_export]
+macro_rules! count1 {
+    (@accum $n:tt) => {
+        $n
+    };
+    (@accum $n:tt $_head:tt $($tail:tt)*) => {{
+        count1!(@accum ($n + 1) $($tail)*)
+    }};
+
+    () => {
+        0
+    };
+    ($($body:tt),*) => {
+        count1!(@accum 0 $($body)*)
+    };
+    ($($body:tt),*,) => {
+        count1!($($body),*)
+    };
+}
+
+#[macro_export]
+macro_rules! hash_map {
+    (@empty $_:expr) => {()};
+    (@count $($pairs:expr)*) =>  {
+        <[()]>::len(&[$(hash_map!(@empty $pairs)),*])
+    };
+
+    ($(($key:expr, $val:expr)),*) => {{
+        use std::collections::HashMap;
+
+        let count = hash_map!(@count $(($key, $val))*);
+        let mut ret = HashMap::with_capacity(count);
+        $(ret.insert($key, $val);)*
+        ret
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count0() {
+        assert_eq!(0, count0!());
+        assert_eq!(1, count0!(1));
+        assert_eq!(2, count0!(1, 2));
+        assert_eq!(2, count0!(1, (1 + 1)));
+    }
+
+    #[test]
+    fn count1() {
+        assert_eq!(0, count1!());
+        assert_eq!(1, count1!(1));
+        assert_eq!(2, count1!(1, 2));
+        assert_eq!(2, count1!(1, (1 + 1)));
+    }
+
+    #[test]
+    fn hash_map() {
+        let expected = {
+            let mut ret = std::collections::HashMap::new();
+            ret.insert("aaa", 3);
+            ret.insert("bbb", 4);
+            ret
+        };
+
+        assert_eq!(expected, hash_map!(("aaa", 2 + 1), ("bbb", 4)));
+    }
+
+    #[test]
+    fn logging() {
+        logging!(foo, bar, baz);
+    }
+}
